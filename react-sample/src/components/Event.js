4bf49936a0f54743f071eebca6d7f3fe6a1478b7
JeremiahMutuc <jeremiahmutuc_p@yahoo.com>
2019-08-12 16:01:35 +0800

    Bind event from Hello love to Goodbye

--- react-sample/src/components/Event.js
+++ react-sample/src/components/Event.js
@@ -2,13 +2,31 @@ import React, {Component} from 'react'
 
 
 class Event extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            message: 'Hello love'
+        }
+    }
+
     event = () =>{
         console.log('Thank you for clicking me')
     }
 
+    bindEvent = () =>{
+        this.setState({
+            message: 'Goodbye'
+        })
+    } 
+
     render(){
         return (
-            <button onClick={this.event}>Just Click Here!</button>
+            <div>
+                <p>{this.state.message}</p>
+                <button onClick={this.event}>Event Handler (console output)</button>
+                <button onClick={this.bindEvent.bind(this)}>Bind event</button>
+            </div>
+            
         )
     }
 }
